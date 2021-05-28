5c5e41fd717d9e7c03e55060a8728e83f2f75e19
Dmitry Kazantsev <57156014+Arof1@users.noreply.github.com>
2021-05-29 03:06:07 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 5, 7, 2, 4, 7, 9, 10, 2, 4 };
+            BinarySearch(arr, 5, 0, arr.Length);
+        }
+
+        static int BinarySearch(int[] array, int searchedValue, int first, int last)
+        {
+            // Ваша реализация здесь
+        }
+    }
+}
