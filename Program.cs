8878bf053bb5a9411709a8dd7518d6f68321f37e
Dmitry Kazantsev <57156014+Arof1@users.noreply.github.com>
2021-05-29 03:07:36 +0500

    done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,7 +16,29 @@ namespace CourseWork
 
         static int BinarySearch(int[] array, int searchedValue, int first, int last)
         {
-            // Ваша реализация здесь
+            if (first > last)
+            {
+                return -1;
+            }
+
+            var middle = (first + last) / 2;
+            var middleValue = array[middle];
+
+            if (middleValue == searchedValue)
+            {
+                return middle;
+            }
+            else
+            {
+                if (middleValue > searchedValue)
+                {
+                    return BinarySearch(array, searchedValue, first, middle - 1);
+                }
+                else
+                {
+                    return BinarySearch(array, searchedValue, middle + 1, last);
+                }
+            }
         }
     }
 }
